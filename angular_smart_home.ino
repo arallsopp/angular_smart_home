038693a89e1cfbceb8709562d752d7d16de75de8
Andy Allsopp <andyallsopp@afinium.local>
2017-12-20 11:57:19 +0000

    COMMON: updated NTP packetBuffer naming to avoid conflict.

--- angular_smart_home.ino
+++ angular_smart_home.ino
@@ -275,7 +275,7 @@ void loop(){
     return;
   }else{
       upnpBroadcastResponder.serverLoop();
-      
+
       first_device->serverLoop();
       /*second_device->serverLoop();*/
       
@@ -288,7 +288,7 @@ void loop(){
 /*-------- NTP code ----------*/
 
 const int NTP_PACKET_SIZE = 48; // NTP time is in the first 48 bytes of message
-byte packetBuffer[NTP_PACKET_SIZE]; //buffer to hold incoming & outgoing packets
+byte ntp_packetBuffer[NTP_PACKET_SIZE]; //buffer to hold incoming & outgoing packets
 
 time_t getNtpTime(){
   IPAddress ntpServerIP; // NTP server's ip address
@@ -306,13 +306,13 @@ time_t getNtpTime(){
     int size = Udp.parsePacket();
     if (size >= NTP_PACKET_SIZE) {
       Serial.println(F("Receive NTP Response"));
-      Udp.read(packetBuffer, NTP_PACKET_SIZE);  // read packet into the buffer
+      Udp.read(ntp_packetBuffer, NTP_PACKET_SIZE);  // read packet into the buffer
       unsigned long secsSince1900;
       // convert four bytes starting at location 40 to a long integer
-      secsSince1900 =  (unsigned long)packetBuffer[40] << 24;
-      secsSince1900 |= (unsigned long)packetBuffer[41] << 16;
-      secsSince1900 |= (unsigned long)packetBuffer[42] << 8;
-      secsSince1900 |= (unsigned long)packetBuffer[43];
+      secsSince1900 =  (unsigned long)ntp_packetBuffer[40] << 24;
+      secsSince1900 |= (unsigned long)ntp_packetBuffer[41] << 16;
+      secsSince1900 |= (unsigned long)ntp_packetBuffer[42] << 8;
+      secsSince1900 |= (unsigned long)ntp_packetBuffer[43];
       return 1 + secsSince1900 - 2208988800UL + timeZoneOffset * SECS_PER_HOUR; //1 is the adjustment for lag.
     }
   }
@@ -323,22 +323,22 @@ time_t getNtpTime(){
 // send an NTP request to the time server at the given address
 void sendNTPpacket(IPAddress &address){
   // set all bytes in the buffer to 0
-  memset(packetBuffer, 0, NTP_PACKET_SIZE);
+  memset(ntp_packetBuffer, 0, NTP_PACKET_SIZE);
   // Initialize values needed to form NTP request
   // (see URL above for details on the packets)
-  packetBuffer[0] = 0b11100011;   // LI, Version, Mode
-  packetBuffer[1] = 0;     // Stratum, or type of clock
-  packetBuffer[2] = 6;     // Polling Interval
-  packetBuffer[3] = 0xEC;  // Peer Clock Precision
+  ntp_packetBuffer[0] = 0b11100011;   // LI, Version, Mode
+  ntp_packetBuffer[1] = 0;     // Stratum, or type of clock
+  ntp_packetBuffer[2] = 6;     // Polling Interval
+  ntp_packetBuffer[3] = 0xEC;  // Peer Clock Precision
   // 8 bytes of zero for Root Delay & Root Dispersion
-  packetBuffer[12] = 49;
-  packetBuffer[13] = 0x4E;
-  packetBuffer[14] = 49;
-  packetBuffer[15] = 52;
+  ntp_packetBuffer[12] = 49;
+  ntp_packetBuffer[13] = 0x4E;
+  ntp_packetBuffer[14] = 49;
+  ntp_packetBuffer[15] = 52;
   // all NTP fields have been given values, now
   // you can send a packet requesting a timestamp:
   Udp.beginPacket(address, 123); //NTP requests are to port 123
-  Udp.write(packetBuffer, NTP_PACKET_SIZE);
+  Udp.write(ntp_packetBuffer, NTP_PACKET_SIZE);
   Udp.endPacket();
 }
 
